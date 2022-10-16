0d12ddc09cdedc0c98fe37d87ef75b2c78282f4e
ChunyanuchBU <110808890+ChunyanuchBU@users.noreply.github.com>
2022-10-16 17:45:53 +0700

    IoT-ESP32-LabSheet-02-63030017

---
+++ 63030017/Lab2-LED-Chasing/LED-Chasing-Challenge-v3/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Espressif/frameworks/esp-idf-v4.4.2/components/bootloader/subproject"
+  "E:/Lab2/LED-Chasing-Challenge-v3/build/bootloader"
+  "E:/Lab2/LED-Chasing-Challenge-v3/build/bootloader-prefix"
+  "E:/Lab2/LED-Chasing-Challenge-v3/build/bootloader-prefix/tmp"
+  "E:/Lab2/LED-Chasing-Challenge-v3/build/bootloader-prefix/src/bootloader-stamp"
+  "E:/Lab2/LED-Chasing-Challenge-v3/build/bootloader-prefix/src"
+  "E:/Lab2/LED-Chasing-Challenge-v3/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "E:/Lab2/LED-Chasing-Challenge-v3/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
