5a3f9efe16f01d8c44a6e01fad5bc882d41a3270
KEDPP <piyathidapho1234@gmail.com>
2022-11-24 01:04:23 +0700

    63030054

---
+++ 63030054/project-topic5.1/main/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "driver/gpio.h"
+
+uint32_t ON_time = 500000;
+uint32_t OFF_time = 0;
+
+uint8_t LEDIO[8] = {23, 22, 1, 3, 21, 19, 18, 5};
+
+void app_main(void)
+{
+    for (uint8_t i = 0; i < 8; i++)
+        gpio_reset_pin(LEDIO[i]);
+
+    for (uint8_t i = 0; i < 8; i++)
+        gpio_set_direction(LEDIO[i], GPIO_MODE_OUTPUT);
+
+    while (true)
+    {
+        for (uint8_t i = 0; i < 8; i++)
+        {
+            gpio_set_level(LEDIO[i], 1);
+            usleep(ON_time);
+            gpio_set_level(LEDIO[i], 0);
+            usleep(OFF_time);
+        }
+        for (uint8_t i = 6; i > 0; i--)
+        {
+            gpio_set_level(LEDIO[i], 1);
+            usleep(ON_time);
+            gpio_set_level(LEDIO[i], 0);
+            usleep(OFF_time);
+        }
+    }
+}
